5bf987e764674d4549bc2e709828c640ab9b3362
ninom5 <nmilas00@fesb.hr>
2026-01-01 13:08:51 +0100

    add BE cnfiguration for register and login of new users

---
+++ apps/api/src/auth/auth.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { AuthController } from './auth.controller';
+import { PrismaService } from 'prisma/prisma.service';
+import { AuthService } from './auth.service';
+import { UserJwtStrategy } from './user.strategy';
+import { UserGuard } from './user.guard';
+
+@Module({
+  imports: [
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '14d' },
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService, UserJwtStrategy, PrismaService, UserGuard],
+  exports: [AuthService, UserGuard],
+})
+export class AuthModule {}
---
+++ apps/api/src/auth/user.strategy.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+type UserJwtPayload = {
+  id: number;
+  email: string;
+  firstName: string;
+  lastName: string;
+  role: 'user';
+};
+
+@Injectable()
+export class UserJwtStrategy extends PassportStrategy(Strategy, 'user') {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: process.env.JWT_SECRET || 'default_secret',
+    });
+  }
+
+  validate(payload): UserJwtPayload {
+    return payload;
+  }
+}
