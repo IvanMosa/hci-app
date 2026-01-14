bcd2a5907facde42c1c6856487cbc3b5d777639f
ninom5 <nmilas00@fesb.hr>
2026-01-15 00:02:19 +0100

    more health checks

--- apps/api/src/health/health.controller.ts
+++ apps/api/src/health/health.controller.ts
@@ -1,7 +1,10 @@
 import { Controller, Get } from '@nestjs/common';
 import {
+  DiskHealthIndicator,
   HealthCheck,
   HealthCheckService,
+  HttpHealthIndicator,
+  MemoryHealthIndicator,
   PrismaHealthIndicator,
 } from '@nestjs/terminus';
 import { PrismaService } from 'src/prisma.service';
@@ -11,6 +14,9 @@ export class HealthController {
   constructor(
     private health: HealthCheckService,
     private prisma: PrismaService,
+    private memory: MemoryHealthIndicator,
+    private disk: DiskHealthIndicator,
+    private network: HttpHealthIndicator,
   ) {}
 
   @Get()
@@ -29,6 +35,16 @@ export class HealthController {
           throw new Error('Database connection failed');
         }
       },
+
+      () => this.memory.checkHeap('memory_heap', 150 * 1024 * 1024),
+
+      () =>
+        this.disk.checkStorage('storage', {
+          path: '/',
+          thresholdPercent: 0.9,
+        }),
+
+      () => this.network.pingCheck('internet', 'https://google.com'),
     ]);
   }
 }
